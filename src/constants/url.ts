73bf53f537e3aacc31f36f0fdc40b458092e3b73
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2024-12-27 19:51:49 +0530

    changed live api url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,7 +4,11 @@ const hostname: HostType = window.location.hostname as HostType;
 
 export const BASE_URL = "http://localhost:5173";
 export const API_URL =
-  hostname == "localhost" ? "http://127.0.0.1:5000/api/v1" : "";
+  hostname == "localhost"
+    ? "http://127.0.0.1:5000/api/v1"
+    : "http://43.204.97.70/server/api/v1/";
 
 export const FILE_URL =
-  hostname == "localhost" ? "http://127.0.0.1:5000/uploads" : "";
+  hostname == "localhost"
+    ? "http://127.0.0.1:5000/uploads"
+    : "http://43.204.97.70/server/uploads";
