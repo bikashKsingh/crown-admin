c1bbaffc5da5fa6db209cf3b5183f36c4d20c6a3
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2024-12-27 20:36:59 +0530

    add homepage to config file

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/admin">
     <App />
     <ToastContainer />
   </BrowserRouter>
